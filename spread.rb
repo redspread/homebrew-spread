2869227b3df717ebde32ef6cbae235c0c24b6a08
Mackenzie Burnett <mackenzie@redspread.com>
2016-03-20 11:38:09 -0700

    spread 0.1.2

--- spread.rb
+++ spread.rb
@@ -1,9 +1,9 @@
 class Spread < Formula
   desc "Deploy Docker to Kubernetes in one command"
   homepage "https://redspread.com"
-  url "https://github.com/redspread/spread/archive/v0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "5eebfc28aa105d69450a4743cf25dc6db91e3f80b9a343cf7f8b14b4c322f55f"
+  url "https://github.com/redspread/spread/archive/0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "3a315135e9508c6ff863b3bd0e17572aabd3168aee2fc3bcc87cd6fb66555ac5"
 
   depends_on "go" => :build
 
