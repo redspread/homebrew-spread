60c57e51261750a838c0a66535a4956481ae1b66
Dan Gillespie <dan@redspread.com>
2016-08-04 18:25:25 -0700

    upgraded spread to have versioning

--- spread.rb
+++ spread.rb
@@ -1,9 +1,9 @@
 class Spread < Formula
   desc "Deploy Docker to Kubernetes in one command"
   homepage "http://redspread.com"
-  url "https://github.com/redspread/spread/archive/0.1.6.tar.gz"
-  version "0.1.6"
-  sha256 "65e0e3531deb3b5c39c68d55605d1b2903b2eb88337e1888aa0f4135392441ec"
+  url "https://github.com/redspread/spread/archive/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "3b56e2d274ba78bdfedf19d6f74876b6ea786fce7e01cb908e320b982869ec"
 
   depends_on "go" => :build
 
@@ -14,7 +14,7 @@ class Spread < Formula
       system "cp", "-r", "cmd", "src/rsprd.com/spread/"
       system "cp", "-r", "cli", "src/rsprd.com/spread/"
       system "cp", "-r", "vendor/", "src"
-      system "go", "build", "rsprd.com/spread/cmd/spread"
+      system "go", "build", "rsprd.com/spread/cmd/spread", "-X", "main.Version=v1.0.0"
       bin.install "spread"
   end
 
